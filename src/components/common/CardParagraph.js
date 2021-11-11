82b51fa8a2049d47825be43ed63dd605e5daf4e4
Adrij Shikhar <adrijshikhar85@gmail.com>
2021-11-12 03:37:28 +0530

    Updates resume Adds experiences Adds skills

---
+++ src/components/common/CardParagraph.js
@@ -0,0 +1,5 @@
+const CardParagraph = ({ children }) => {
+  return <p className="para-content">{children}</p>;
+};
+
+export default CardParagraph;
--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -10,14 +10,133 @@ const Experience = () => {
         <h2 class="mb-5">Experience</h2>
 
         <ExpCard
+          postition="SDE Intern | Hevo Data"
+          location="Bangalore"
+          date="Sep 2021 - Oct 2021"
+        >
+          <ExpCard.Content>
+            <ExpCard.Paragraph>
+              Load data from any source into your warehouse
+            </ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Added support for ‘wal2json’ output plugin for postgres
+                  logical replication.
+                </li>
+                <li>
+                  Reduced WAL processing time taken by the platform, by 60%.
+                </li>
+                <li>
+                  Implemented dynamic plugin selection using Guice dependency
+                  injection.
+                </li>
+                <li>
+                  Ensured stability by benchmarks and unit tests. Tech Stack:
+                  Java
+                </li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
+        </ExpCard>
+
+        <ExpCard
+          postition="SDE Intern | MTX Global"
+          location="Hydrabad"
+          date="Jun 2021 - Aug 2021"
+        >
+          <ExpCard.Content>
+            <ExpCard.Paragraph>GO DIGITAL. THINK HUMAN.</ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Worked on curating ETL data pipeline from concept to proof of
+                  concept.
+                </li>
+                <li>
+                  Implemented on-demand data transformations using Apache Spark,
+                  and streaming the same using Apache Kafka onto Google Cloud
+                  Platform.
+                </li>
+                <li>
+                  Containerized individual components of the pipeline for better
+                  development and deployment.
+                </li>
+                <li>
+                  Solely configured and maintained pipeline on Google K8s
+                  Engine.
+                </li>
+                <li>
+                  Orchestrated data visualization service to configure overlayed
+                  charts.
+                </li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
+        </ExpCard>
+
+        <ExpCard
+          postition="Full Stack Developer Intern | Triomics"
+          location="Gurgaon"
+          date="Apr 2021 - May 2021"
+        >
+          <ExpCard.Content>
+            <ExpCard.Paragraph>
+              Making Clinical Trials faster and transparent
+            </ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Collaborated with the core founding team on the initial stages
+                  of the platform.
+                </li>
+                <li>
+                  Setup infrastructure for the applications, keeping scalability
+                  and security into account.
+                </li>
+                <li>
+                  Developed management dashboard service for micro and macro
+                  level user access across the apps.
+                </li>
+                <li>
+                  Customized D3 for visualization of data, fitting our use case.
+                </li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
+        </ExpCard>
+
+        <ExpCard
           postition="SDE Intern | Rephrase.ai"
           location="Bangalore"
           date="Oct 2020 - March 2021"
         >
-          <p class="para-content">
-            Use Generative AI to address millions of customers personally,
-            through videos.
-          </p>
+          <ExpCard.Content>
+            <ExpCard.Paragraph>
+              Use Generative AI to address millions of customers personally,
+              through videos.
+            </ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Ensured stability of the product by integrating tests and
+                  error handling.
+                </li>
+                <li>
+                  Optimized uploading and validating data from user's end.
+                </li>
+                <li>
+                  Implemented Stripe, Sentry, Clickup for better development
+                  cycle.
+                </li>
+                <li>
+                  Integrated continuous integration and ensured continuous
+                  delivery among various services.
+                </li>
+                <li> Worked on graphene to optimize API performance.</li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
         </ExpCard>
 
         <ExpCard
@@ -25,9 +144,33 @@ const Experience = () => {
           location="Bangalore"
           date="Apr 2020 - Jun 2020"
         >
-          <p class="para-content">
-            Helping construction contractors track realtime on-site progress
-          </p>
+          <ExpCard.Content>
+            <ExpCard.Paragraph>
+              Helping construction contractors track realtime on-site progress
+            </ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Implemented core features and structure from concept through
+                  deployment.
+                </li>
+                <li>
+                  Introduced REST API', server-side pagination and JWT based
+                  authentication system.
+                </li>
+                <li>
+                  Standardized UI libraries by enclosing them in highly
+                  customizable wrapper for code reusability.
+                </li>
+                <li>Assessed UX and UI designs for technical feasibility.</li>
+                <li>Developed standard and ad hoc report in table format.</li>
+                <li>
+                  Collaborated with product team members to implement new
+                  feature developments.
+                </li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
         </ExpCard>
 
         <ExpCard
@@ -35,7 +178,26 @@ const Experience = () => {
           location="IIT Roorkee"
           date="Jan 2019 - Present"
         >
-          <p class="para-content">Think. Build. Ship</p>
+          <ExpCard.Content>
+            <ExpCard.Paragraph>Think. Build. Ship</ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Under the hood of the group, we promote technical culture on
+                  the campus by conducting hackathons, lecture series, and
+                  competitions.
+                </li>
+                <li>
+                  Responsible for maintaining current applications and server
+                  management.
+                </li>
+                <li>
+                  Mentored freshmen students in their projects for the Winter of
+                  Code program.
+                </li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
         </ExpCard>
 
         <ExpCard
@@ -43,7 +205,18 @@ const Experience = () => {
           location="IIT Roorkee"
           date="Feb 2019 - Feb 2020"
         >
-          <p class="para-content">#inspiringinnovation</p>
+          <ExpCard.Content>
+            <ExpCard.Paragraph>#inspiringinnovation</ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Conducted meetings, hands-on workshops and events on various
+                  topics related to entrepreneurship and startups.
+                </li>
+                <li>Participated in various case studies regarding SaaS.</li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
         </ExpCard>
 
         <ExpCard
@@ -51,7 +224,9 @@ const Experience = () => {
           location="JB Online Media, LLC"
           date="Feb 2015 - Present"
         >
-          <p class="para-content">Tech Fest</p>
+          <ExpCard.Content>
+            <ExpCard.Paragraph>Open Source Android forum</ExpCard.Paragraph>
+          </ExpCard.Content>
         </ExpCard>
 
         <ExpCard
@@ -59,7 +234,19 @@ const Experience = () => {
           location="IIT Roorkee"
           date="Dec 2018 - Jan 2020"
         >
-          <p class="para-content">IIT Roorkee Tech Fest</p>
+          <ExpCard.Content>
+            <ExpCard.Paragraph>IIT Roorkee Tech Fest</ExpCard.Paragraph>
+            <ExpCard.Paragraph>
+              <ul>
+                <li>
+                  Worked in a 3-tier team of 5+ executive members, associate
+                  members, and co-coordinators to establish web presence of
+                  Cognizance 2019 and 2020.
+                </li>
+                <li>Worked as a Manager Web in 2019 and 2020</li>
+              </ul>
+            </ExpCard.Paragraph>
+          </ExpCard.Content>
         </ExpCard>
       </div>
     </section>
