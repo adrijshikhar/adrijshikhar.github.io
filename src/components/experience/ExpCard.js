710305e49149931323ffb2f8529f0a59fcd073a2
Adrij Shikhar <adrijshikhar85@gmail.com>
2021-11-13 02:21:45 +0530

    Adds links in company name

--- src/components/experience/ExpCard.js
+++ src/components/experience/ExpCard.js
@@ -1,11 +1,23 @@
 import CardContent from '../common/CardContent';
 import CardParagraph from '../common/CardParagraph';
 
-const ExpCard = ({ postition, location, date, ...props }) => {
+const ExpCard = ({
+  postition,
+  companyName,
+  companyLink,
+  location,
+  date,
+  ...props
+}) => {
   return (
     <div class="resume-item d-flex justify-content-between mb-5">
       <div class="resume-content mr-3 ">
-        <h3 class="mb-1">{postition}</h3>
+        <h3 class="mb-1">
+          {postition} |{' '}
+          <a href={companyLink} target="_blank">
+            {companyName}
+          </a>
+        </h3>
         <div class="subheading mb-3 font-italic">{location}</div>
         {props.children}
       </div>
